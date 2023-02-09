fd1b860ce4819baf518b318003f14bcdb90eb69e
alphaf91 <yenphan401@gmail.com>
2023-02-09 11:58:36 +0700

    0902023

--- OOP_concept/src/ShapeDemo3.java
+++ OOP_concept/src/ShapeDemo3.java
@@ -1,8 +1,17 @@
-
+import javax.swing.*;
 public class ShapeDemo3 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double[] radius = new double[5]; 
+		double[] height = new double[5];
+		for(int i=0;i<4;i++) {
+			radius[i] = Double.parseDouble(JOptionPane.showInputDialog((i+1)+" . 1Iput radius"));
+			height[i] = Double.parseDouble(JOptionPane.showInputDialog((i+1)+" . 1Iput height"));
+		}
+		for(int i=0;i<4;i++) {
+			Cylinder cd = new Cylinder(radius[i],height[i]);
+			JOptionPane.showMessageDialog(null,"Cylinder"+(i+1)+", volume"+cd.getVolume());
+		}
 
 	}
 
