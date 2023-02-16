71a5258d63f3b2fb68c86c6dcacb05582eaae10c
alphaf91 <118412809+alphaf91@users.noreply.github.com>
2023-02-16 11:57:55 +0700

    Add files via upload

--- labsheet12/src/labsheet12/ATMData.java
+++ labsheet12/src/labsheet12/ATMData.java
@@ -1,5 +1,17 @@
 package labsheet12;
 
 public class ATMData {
+	String accountNumber;
+	String password;
+	
+	public static ATMData(String,String) {
+		
+	}
+	public String getID() {
+		
+	}
+	public String getPass() {
+		
+	}
 
 }
--- labsheet12/src/labsheet12/Employee.java
+++ labsheet12/src/labsheet12/Employee.java
@@ -1,5 +1,18 @@
 package labsheet12;
 
 public class Employee {
+	private String dept;
+	
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public String getDept() {
+		return this.dept =dept;
+	}
+	public void header() {
+		for(int i=1;i<80;i++) {
+			System.out.print("*");
+		}
+	}
 
 }
